d27bb7ecf68f4bfbd423e4204aa09e16658b64d5
Sahil <81418397+somilkhan@users.noreply.github.com>
2026-08-21 06:59:37 +0530

    chore: restore production build checks

--- next.config.js
+++ next.config.js
@@ -12,8 +12,6 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
 }
 
 module.exports = nextConfig
